caa0ba83cb29642bb25abb79b5a21242aeca358d
Ankit Seth <iam4nkit@gmail.com>
2021-11-18 20:31:02 +0530

    Add files via upload

---
+++ JAVA/Java Stdin and Stdout II.java
@@ -0,0 +1,20 @@
+//Problem Link: https://www.hackerrank.com/challenges/java-stdin-stdout/
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d= scan.nextDouble();
+        scan.nextLine();
+        String s= scan.nextLine();
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
